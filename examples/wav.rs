718e45fc6628b2a64718609f9cc9752e4977b5d1
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2026-03-26 19:44:47 -0700

    Simplify library and make example recursive
    No longer have out of bounds check in library

--- examples/wav.rs
+++ examples/wav.rs
@@ -3,76 +3,61 @@ use std::{
     io::{Read, Seek, SeekFrom},
 };
 
-use pure_riff::{Id, ProcessDataOutput, ReadInstructions, RiffChunkHeader, RiffChunksParser};
-use zerocopy::transmute;
+use pure_riff::{Id, RiffChunksParser};
 
 fn main() {
     let mut file =
         File::open("Warriyo, Laura Brehm - Mortals (feat. Laura Brehm) [NCS Release].wav").unwrap();
+    let file_len = u32::try_from(file.metadata().unwrap().len()).unwrap();
 
-    // The entire WAVE file is a RIFF chunk
-    let mut header_buffer = [Default::default(); size_of::<RiffChunkHeader>()];
-    file.read_exact(&mut header_buffer).unwrap();
-    let header: RiffChunkHeader = transmute!(header_buffer);
-
-    let header_id = str::from_utf8(&header.chunk_id);
-    println!("{header:#?} {header_id:?}");
-
-    let root_container_info = header.container_info().unwrap().unwrap();
-    // Read the container id
-    let mut id_buffer = [Default::default(); size_of::<Id>()];
-    file.seek(SeekFrom::Start(root_container_info.id.position.into()))
-        .unwrap();
-    file.read_exact(&mut id_buffer).unwrap();
-
-    let container_id = str::from_utf8(&id_buffer);
-    println!("container id: {container_id:?}");
-
-    let mut parser = RiffChunksParser::new(root_container_info.sub_chunks.len);
-    loop {
-        let ReadInstructions {
-            position,
-            prefetch_len: _prefetch_len,
-        } = parser.read_instructions();
-        file.seek(SeekFrom::Start(
-            (root_container_info.sub_chunks.position + position).into(),
-        ))
-        .unwrap();
+    let mut depth = 0;
+    let mut container_end_stack = vec![file_len];
+    let mut parser = RiffChunksParser::new(0);
+    while let Some(current_container_end) = container_end_stack.last() {
+        if parser.position() == *current_container_end {
+            if container_end_stack.pop().is_some() {
+                depth -= 1;
+                continue;
+            } else {
+                break;
+            }
+        }
+        file.seek(SeekFrom::Start((parser.position()).into()))
+            .unwrap();
         let mut buffer = [Default::default(); RiffChunksParser::MIN_READ_BUFFER_LEN];
         file.read_exact(&mut buffer).unwrap();
-        let ProcessDataOutput {
-            chunk_info,
-            continue_parsing,
-        } = parser.process_data(buffer);
+        let chunk_info = parser.process_data(buffer);
 
-        let chunk_id = str::from_utf8(&chunk_info.header.chunk_id);
-        println!("{chunk_info:#?} {chunk_id:?}");
-        if chunk_info.valid_len
-            && let Some(container_info) = chunk_info.header.container_info()
-        {
+        let chunk_id = str::from_utf8(&chunk_info.header.chunk_id).unwrap();
+        let chunk_len = chunk_info.header.chunk_len.get();
+        for _ in 0..depth {
+            print!("  ");
+        }
+        println!("{chunk_id} ({chunk_len} B)");
+        if let Some(container_info) = chunk_info.header.container_info() {
             let container_info = container_info.unwrap();
             // Read the container id
             let mut id_buffer = [Default::default(); size_of::<Id>()];
+            depth += 1;
+
             file.seek(SeekFrom::Start(
-                (root_container_info.sub_chunks.position
-                    + chunk_info.position
-                    + container_info.id.position)
-                    .into(),
+                (chunk_info.position + container_info.id_position).into(),
             ))
             .unwrap();
             file.read_exact(&mut id_buffer).unwrap();
-
-            let container_id = str::from_utf8(&id_buffer);
-            println!("container id: {container_id:?}");
-        }
-
-        match continue_parsing.unwrap() {
-            Some(next_parser) => {
-                parser = next_parser;
+            let id = str::from_utf8(&id_buffer).unwrap();
+            for _ in 0..depth {
+                print!("  ");
             }
-            None => {
-                break;
-            }
+            println!("{id}");
+
+            parser =
+                RiffChunksParser::new(chunk_info.position + container_info.sub_chunks.position);
+            container_end_stack.push(
+                chunk_info.position
+                    + container_info.sub_chunks.position
+                    + container_info.sub_chunks.len,
+            );
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -26,8 +26,10 @@ pub struct SubChunksError;
 
 #[derive(Debug)]
 pub struct ContainerInfo {
-    /// The identifier of the container chunk, such as "AVI " or "WAVE".
-    pub id: SliceU32,
+    /// See the [`Id`] type for the len of the id.
+    /// The id is different from the container chunk's id.
+    /// Sample values for the id itself: "AVI ", "WAVE".
+    pub id_position: u32,
     /// Position relative to position of the container chunk
     pub sub_chunks: SliceU32,
 }
@@ -41,10 +43,7 @@ impl RiffChunkHeader {
                     let chunk_header_len_u32 = u32::try_from(size_of::<RiffChunkHeader>()).unwrap();
                     let id_len_u32 = u32::try_from(size_of::<Id>()).unwrap();
                     Ok(ContainerInfo {
-                        id: SliceU32 {
-                            position: chunk_header_len_u32,
-                            len: id_len_u32,
-                        },
+                        id_position: chunk_header_len_u32,
                         sub_chunks: SliceU32 {
                             position: chunk_header_len_u32 + id_len_u32,
                             len: self.chunk_len.get() - id_len_u32,
@@ -59,10 +58,6 @@ impl RiffChunkHeader {
     }
 }
 
-/// The data would be out of bounds.
-// #[derive(Debug, Clone, Copy)]
-// pub struct ParseDataInfoError;
-
 #[derive(Debug, Clone, Copy)]
 pub struct SliceU32 {
     pub position: u32,
@@ -71,46 +66,10 @@ pub struct SliceU32 {
 
 #[derive(Debug)]
 pub struct ParsedRiffChunkInfo {
-    // /// Usually a (4 byte) ASCII string, such as "RIFF" or "fmt ".
-    // pub id: Id,
-    // pub data_info: Result<SliceU32, ParseDataInfoError>,
     pub header: RiffChunkHeader,
     /// Position of this header relative to the start of the list of chunks.
     pub position: u32,
-    /// `true` if this chunk (including its data) can fit in the list of chunks.
-    pub valid_len: bool,
 }
-
-// impl ParsedRiffChunkInfo {
-//     pub fn container_info(&self) -> Option<Result<ContainerInfo, ContainerInfoError>> {
-//         match &self.id {
-//             b"RIFF" | b"LIST" => Some({
-//                 match self.data_info {
-//                     Ok(data_info) => {
-//                         let len_usize = usize::try_from(data_info.len).unwrap();
-//                         if len_usize >= size_of::<Id>() {
-//                             let id_len_u32 = u32::try_from(size_of::<Id>()).unwrap();
-//                             Ok(ContainerInfo {
-//                                 id: SliceU32 {
-//                                     position: data_info.position,
-//                                     len: id_len_u32,
-//                                 },
-//                                 sub_chunks: SliceU32 {
-//                                     position: data_info.position + id_len_u32,
-//                                     len: data_info.len - id_len_u32,
-//                                 },
-//                             })
-//                         } else {
-//                             Err(ContainerInfoError)
-//                         }
-//                     }
-//                     Err(_) => Err(ContainerInfoError),
-//                 }
-//             }),
-//             _ => None,
-//         }
-//     }
-// }
 
 /// For the required len, use [`RiffChunksParser::MIN_READ_BUFFER_LEN`].
 pub struct ReadInstructions {
@@ -131,46 +90,31 @@ pub struct ProcessDataOutput {
 }
 
 pub struct RiffChunksParser {
-    len: u32,
     position: u32,
 }
 
 impl RiffChunksParser {
-    pub fn new(len: u32) -> Self {
-        Self { len, position: 0 }
+    /// If you want to read from the beginning of the list of chunks, use position `0`.
+    pub fn new(position: u32) -> Self {
+        Self { position }
+    }
+
+    pub fn position(&self) -> u32 {
+        self.position
     }
 
     pub const MIN_READ_BUFFER_LEN: usize = size_of::<RiffChunkHeader>();
 
-    pub fn read_instructions(&self) -> ReadInstructions {
-        ReadInstructions {
-            position: self.position,
-            prefetch_len: self.len - self.position,
-        }
-    }
-
-    pub fn process_data(self, data: [u8; Self::MIN_READ_BUFFER_LEN]) -> ProcessDataOutput {
+    pub fn process_data(&mut self, data: [u8; Self::MIN_READ_BUFFER_LEN]) -> ParsedRiffChunkInfo {
         let chunk_header: RiffChunkHeader = transmute!(data);
         let data_len = chunk_header.chunk_len.get();
         let header_len = u32::try_from(size_of::<RiffChunkHeader>()).unwrap();
+        let chunk_position = self.position;
         // The specification says that there is a padding byte if the data len is odd
-        let new_position = self.position + header_len + data_len.next_multiple_of(2);
-        ProcessDataOutput {
-            chunk_info: ParsedRiffChunkInfo {
-                header: chunk_header,
-                position: self.position,
-                valid_len: new_position <= self.len,
-            },
-            continue_parsing: if new_position + header_len <= self.len {
-                Ok(Some(Self {
-                    len: self.len,
-                    position: new_position,
-                }))
-            } else if new_position == self.len {
-                Ok(None)
-            } else {
-                Err(ParseChunksError)
-            },
+        self.position += header_len + data_len.next_multiple_of(2);
+        ParsedRiffChunkInfo {
+            header: chunk_header,
+            position: chunk_position,
         }
     }
 }
